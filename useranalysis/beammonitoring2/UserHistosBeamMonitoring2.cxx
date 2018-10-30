2e742f55580ba948af0c8aa3b6be3090909364bd
Daria <daria@localhost.localdomain>
2018-10-30 12:12:52 +0300

    Added projection from MWPC to any plane based on Vratislav beam monitoring 21 Oct, second try

---
+++ useranalysis/beammonitoring2/UserHistosBeamMonitoring2.cxx
@@ -0,0 +1,88 @@
+#include "UserHistosBeamMonitoring2.h"
+
+// ROOT
+#include <TH1D.h>
+#include <TH2D.h>
+
+// Go4
+#include <TGo4Analysis.h>
+#include <TGo4Picture.h>
+
+// Project
+#include "setupconfigcppwrapper/SetupConfiguration.h"
+
+UserHistosBeamMonitoring2::UserHistosBeamMonitoring2()
+{
+	TGo4Analysis* a = TGo4Analysis::Instance();
+
+	fMWPCwire[0] = a->MakeTH1('I', "BM2/wires/X1w", "My new histogram", 32, 0, 32);
+	fMWPCwire[1] = a->MakeTH1('I', "BM2/wires/Y1w", "My new histogram", 32, 0, 32);
+	fMWPCwire[2] = a->MakeTH1('I', "BM2/wires/X2w", "My new histogram", 32, 0, 32);
+	fMWPCwire[3] = a->MakeTH1('I', "BM2/wires/Y2w", "My new histogram", 32, 0, 32);
+
+	fMWPCmmW[0] = a->MakeTH1('I', "BM2/X1mmW", "X1 position in mm", 46, -23., 23.);
+	fMWPCmmW[1] = a->MakeTH1('I', "BM2/Y1mmW", "Y1 position in mm", 46, -23., 23.);
+	fMWPCmmW[2] = a->MakeTH1('I', "BM2/X2mmW", "X2 position in mm", 46, -23., 23.);
+	fMWPCmmW[3] = a->MakeTH1('I', "BM2/Y2mmW", "Y2 position in mm", 46, -23., 23.);
+
+	fMWPCmmC[0] = a->MakeTH1('I', "BM2/X1mmC", "X1 position in mm", 46, -23., 23.);
+	fMWPCmmC[1] = a->MakeTH1('I', "BM2/Y1mmC", "Y1 position in mm", 46, -23., 23.);
+	fMWPCmmC[2] = a->MakeTH1('I', "BM2/X2mmC", "X2 position in mm", 46, -23., 23.);
+	fMWPCmmC[3] = a->MakeTH1('I', "BM2/Y2mmC", "Y2 position in mm", 46, -23., 23.);
+
+	fMultW[0] = a->MakeTH1('I', "BM2/Mult/multX1w", "multiplicity x1", 32, 0, 32);
+	fMultW[1] = a->MakeTH1('I', "BM2/Mult/multY1w", "multiplicity y1", 32, 0, 32);
+	fMultW[2] = a->MakeTH1('I', "BM2/Mult/multX2w", "multiplicity x2", 32, 0, 32);
+	fMultW[3] = a->MakeTH1('I', "BM2/Mult/multY2w", "multiplicity y2", 32, 0, 32);
+
+	fMWPCcluster[0] = a->MakeTH1('I', "BM2/wires/X1c", "My new histogram", 32, 0, 32);
+	fMWPCcluster[1] = a->MakeTH1('I', "BM2/wires/Y1c", "My new histogram", 32, 0, 32);
+	fMWPCcluster[2] = a->MakeTH1('I', "BM2/wires/X2c", "My new histogram", 32, 0, 32);
+	fMWPCcluster[3] = a->MakeTH1('I', "BM2/wires/Y2c", "My new histogram", 32, 0, 32);
+
+	fMultC[0] = a->MakeTH1('I', "BM2/Mult/multX1c", "multiplicity x1", 32, 0, 32);
+	fMultC[1] = a->MakeTH1('I', "BM2/Mult/multY1c", "multiplicity y1", 32, 0, 32);
+	fMultC[2] = a->MakeTH1('I', "BM2/Mult/multX2c", "multiplicity x2", 32, 0, 32);
+	fMultC[3] = a->MakeTH1('I', "BM2/Mult/multY2c", "multiplicity y2", 32, 0, 32);
+
+	fMWPCwireProfile[0] = a->MakeTH2('I', "BM2/wires/XY1wire", "Y1 vs X1: 1 fired wire", 32, 0, 32, 32, 0, 32,"X1","Y1");
+	fMWPCwireProfile[1] = a->MakeTH2('I', "BM2/wires/XY2wire", "Y2 vs X2: 1 fired wire", 32, 0, 32, 32, 0, 32,"X2","Y2");
+
+	fMWPCclusterProfile[0] = a->MakeTH2('I', "BM2/wires/XY1cluster", "Y1 vs X1: 1 fired wire", 32, 0, 32, 32, 0, 32,"X1","Y1");
+	fMWPCclusterProfile[1] = a->MakeTH2('I', "BM2/wires/XY2cluster", "Y2 vs X2: 1 fired wire", 32, 0, 32, 32, 0, 32,"X2","Y2");
+
+	fMWPCProfileW[0] = a->MakeTH2('I', "BM2/XY1w", "Y1 vs X1: mm", 46, -23., 23., 46, -23., 23.,"X1","Y1");
+	fMWPCProfileW[1] = a->MakeTH2('I', "BM2/XY2w", "Y2 vs X2: mm", 46, -23., 23., 46, -23., 23.,"X2","Y2");
+	fMWPCProfileW[2] = a->MakeTH2('I', "BM2/XYtw", "Yt vs Xt: mm", 46, -23., 23., 46, -23., 23.,"Xt","Yt");
+
+	fMWPCProfileC[0] = a->MakeTH2('I', "BM2/XY1c", "Y1 vs X1: mm", 46, -23., 23., 46, -23., 23.,"X1","Y1");
+	fMWPCProfileC[1] = a->MakeTH2('I', "BM2/XY2c", "Y2 vs X2: mm", 46, -23., 23., 46, -23., 23.,"X2","Y2");
+	fMWPCProfileC[2] = a->MakeTH2('I', "BM2/XYtc", "Yt vs Xt: mm", 46, -23., 23., 46, -23., 23.,"Xt","Yt");
+
+	fMWPCwireEff = a->MakeTH1('D', "BM2/wireEff", "Multiplicity 1 in wires", 5, 0, 5);
+	fMWPCclusterEff = a->MakeTH1('D', "BM2/clusterEff", "Multiplicity 1 in clusters", 5, 0, 5);
+
+	//daria
+	//fTargProjDaria = a->MakeTH2('I',"BM2/TargProjDaria","Yt vs Xt: mm", 46, -23., 23., 46, -23., 23.,"Xt","Yt");
+	fPlaneProjW = a->MakeTH2('I',"BM2/PlaneProjW","Proj. to plane at the dist. from target, wires", 46, -23., 23., 46, -23., 23.,"X","Y");
+	fPlaneProjC = a->MakeTH2('I',"BM2/PlaneProjC","Proj. to plane at the dist. from target, clusters", 46, -23., 23., 46, -23., 23.,"X","Y");
+
+	fMWPCtime[0] = a->MakeTH1('I',"BM2/fMWPCtime/time_MWPC0","some time 0", 500, 0, 10e3);
+	fMWPCtime[1] = a->MakeTH1('I',"BM2/fMWPCtime/time_MWPC1","some time 1", 500, 0, 10e3);
+	fMWPCtime[2] = a->MakeTH1('I',"BM2/fMWPCtime/time_MWPC2","some time 2", 500, 0, 10e3);
+	fMWPCtime[3] = a->MakeTH1('I',"BM2/fMWPCtime/time_MWPC3","some time 3", 500, 0, 10e3);
+	TGo4Picture *pic = new TGo4Picture("pic","picpicpic");
+	pic->AddH1(fMWPCwireEff);
+	pic->SetDrawOption("col");
+	a->AddPicture(pic);
+}
+
+UserHistosBeamMonitoring2::~UserHistosBeamMonitoring2()
+{
+	/**
+	 * Not 100% sure, but it looks as if you do not need to delete your histograms yourself.
+	 * TGo4AnalysisObjectManager, as part of the singleton TGo4Analysis, will take care.
+	 */
+}
+
+ClassImp(UserHistosBeamMonitoring2)
---
+++ useranalysis/beammonitoring2/UserHistosBeamMonitoring2.h
@@ -0,0 +1,57 @@
+/**
+
+	@class UserHistosBeamMonitoring
+
+*/
+
+#ifndef USERHISTOSBEAMMONITORING2_H
+#define USERHISTOSBEAMMONITORING2_H
+
+#include <TObject.h> // mother class
+
+// STD
+#include <map>
+#include <fstream>
+	
+class TH1;
+class TH2;
+
+class UserHistosBeamMonitoring2 : public TObject
+{
+public:
+	UserHistosBeamMonitoring2();
+	~UserHistosBeamMonitoring2();
+
+//	void GenerateAutoHistos(void);
+
+	TH1* fMWPCwire[4];		//!
+	TH1* fMultW[4];		//!
+	TH2* fMWPCwireProfile[2];	//!
+
+	TH1* fMWPCcluster[4];		//!
+	TH1* fMultC[4];		//!
+	TH2* fMWPCclusterProfile[2];	//!
+
+	TH1* fMWPCmmW[4];		//!
+	TH2* fMWPCProfileW[3];	//!
+
+	TH1* fMWPCmmC[4];		//!
+	TH2* fMWPCProfileC[3];	//!
+
+	TH1* fMWPCwireEff;		//!
+	TH1* fMWPCclusterEff;			//!
+
+	//daria 
+	TH1* fMWPCtime[4];		//!
+	TH2* fPlaneProjW;		//!
+	TH2* fPlaneProjC;		//!
+
+	/**
+	 * Automatically generated histograms
+	 */
+//	std::map<unsigned int, TH1*> fAutoHistos_Beam; //!
+
+	ClassDef(UserHistosBeamMonitoring2, 1);
+};
+
+#endif // USERHISTOSBEAMMONITORING2_H
